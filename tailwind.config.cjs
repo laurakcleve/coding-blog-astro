da95b6ac1aa122704b1adbb757ec48f022894713
laura <laurakcleve@gmail.com>
2023-02-09 15:22:09 -0700

    customize, add Tailwind Typography

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,8 +8,45 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ['Inter', ...defaultTheme.fontFamily.sans],
+        mono: ['Fira Code', ...defaultTheme.fontFamily.mono],
       },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            color: theme('colors.zinc[50]'),
+            fontWeight: '300',
+            a: {
+              color: theme('colors.emerald[300]'),
+              '&:hover': {
+                color: theme('colors.emerald[200]'),
+              },
+              textDecoration: 'none',
+            },
+            code: {
+              '&::before': {
+                display: 'none',
+              },
+              '&::after': {
+                display: 'none',
+              },
+              fontWeight: '300',
+              paddingLeft: '2px',
+              paddingRight: '2px',
+              color: theme('colors.blue[100]'),
+              backgroundColor: theme('colors.zinc[900]'),
+              borderRadius: '4px',
+            },
+            blockquote: {
+              fontStyle: 'none',
+              quotes: 'none',
+              fontWeight: '300',
+              color: theme('colors.blue[200]'),
+              borderLeftColor: theme('colors.blue[300]'),
+            },
+          },
+        },
+      }),
     },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/typography')],
 }
